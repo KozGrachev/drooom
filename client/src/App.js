9752a8926136fb8aaa6852ada3575df70a2ddee6
Konstantin Grachev <kgrachev@gmail.com>
2020-11-27 01:15:08 +0000

    Plays kick on click

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,39 @@
 import './style/App.scss';
 import { useState } from 'react';
 import * as Tone from 'tone';
-import { Note, Scale } from "@tonaljs/tonal";
+// import { Note, Scale } from "@tonaljs/tonal";
 import { Drums } from './drums'
 
 function App() {
-  const [note1, setNote1] = useState('c4');
-  const [notesSequence, setNoteSequence] = useState(() => {
-    return Scale.scale('C pentatonic').notes;
-  });
+  // const [note1, setNote1] = useState('c4');
+  // const [notesSequence, setNoteSequence] = useState(() => {
+  //   return Scale.scale('C pentatonic').notes;
+  // });
 
-  function playSound() {
+  // function playSound() {
     //create a synth and connect it to the main output (your speakers)
-    const synthFM = new Tone.FMSynth().toDestination();
-    const synthAM = new Tone.AMSynth().toDestination();
-    const synthPoly = new Tone.PolySynth(Tone.Synth).toDestination();
-    const now = Tone.now();
+    // const synthFM = new Tone.FMSynth().toDestination();
+    // const synthAM = new Tone.AMSynth().toDestination();
+    // const synthPoly = new Tone.PolySynth(Tone.Synth).toDestination();
+    // const now = Tone.now();
 
-    synthPoly.triggerAttack("D4", now);
-    synthPoly.triggerAttack("F4", now + 0.5);
-    synthPoly.triggerAttack("A4", now + 1);
-    synthPoly.triggerAttack("C5", now + 1.5);
-    synthPoly.triggerAttack("E5", now + 2);
-    synthPoly.triggerRelease(["D4", "F4", "A4", "C5", "E5"], now + 4);
+    // synthPoly.triggerAttack("D4", now);
+    // synthPoly.triggerAttack("F4", now + 0.5);
+    // synthPoly.triggerAttack("A4", now + 1);
+    // synthPoly.triggerAttack("C5", now + 1.5);
+    // synthPoly.triggerAttack("E5", now + 2);
+    // synthPoly.triggerRelease(["D4", "F4", "A4", "C5", "E5"], now + 4);
 
-    const loopA = new Tone.Loop(time => {
-      synthFM.triggerAttackRelease('C2', '8n', time);
-    }, '4n').start(0);
-    const loopB = new Tone.Loop(time => {
-      synthAM.triggerAttackRelease('C3', '8n', time);
-    });
+    // const loopA = new Tone.Loop(time => {
+    //   synthFM.triggerAttackRelease('C2', '8n', time);
+    // }, '4n').start(0);
+    // const loopB = new Tone.Loop(time => {
+    //   synthAM.triggerAttackRelease('C3', '8n', time);
+    // });
 
 
-    Tone.Transport.start();
-    Tone.Transport.bpm.value = 200;
+    // Tone.Transport.start();
+    // Tone.Transport.bpm.value = 200;
 
     //play a middle 'C' for the duration of an 8th note
     // synth.triggerAttackRelease("C4", "8n");
@@ -41,24 +41,24 @@ function App() {
     // synth.triggerAttackRelease('e4b', '8n', now + 0.5);
     // synth.triggerAttackRelease('g4', '8n', now + 1);
     // synth.triggerRelease(now + 1);
-  }
+  // }
 
-  function stopSound() {
-    Tone.Transport.stop();
-  }
+  // function stopSound() {
+  //   Tone.Transport.stop();
+  // }
 
   function playNote(event) {
 
   }
 
-  function renderKeys() {
+  // function renderKeys() {
 
-    return notesSequence.map((note) => {
+  //   return notesSequence.map((note) => {
 
-      return <div className="note" id={note} vaue={note} onClick={playNote}>{note}</div>
+  //     return <div className="note" id={note} vaue={note} onClick={playNote}>{note}</div>
 
-    })
-  }
+  //   })
+  // }
 
 
 
--- client/src/drums.js
+++ client/src/drums.js
@@ -1,19 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import { Circle } from './Circle'
 // import './style/Step.scss'
 import { Step } from './Step'
 import './style/drums.scss'
+import * as Tone from 'tone';
+import { Sampler } from 'tone';
 import { v4 } from 'uuid'
+import kick from './assets/kick.mp3'
+// import { Tone } from 'tone/build/esm/core/Tone';
 
 export function Drums () {
+  const [step, setStep] = useState(0);
+  const steps = [1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0];
 
   // Consider creating the sequencer here and receiving active/inactive
   // events from Notes
   // Pass the handleClick function down to notes
 
-  function handleNoteClick ( note ) {
+  function handleNoteClick (note) {
     console.log('Clicked: ', note.name, note.stepNum, note.active);
   }
+
+  function createDrumMachine () {
+    console.log('CLICKED DRUM MACHINE');
+
+    const kick = new Tone.Player('../assets/kick.wav').toDestination();
+    // const snare = new Tone.Player('../assets/snare.wav').toDestination();
+    // const hhc = new Tone.Player('../assets/hhc.wav').toDestination();
+    // const hho = new Tone.Player('../assets/hho.wav').toDestination();
+    // const rim = new Tone.Player('../assets/rim.wav').toDestination();
+
+    Tone.Transport.scheduleRepeat(playStep, '16n');
+    Tone.Transport.start();
+
+    let stp = 0;
+
+
+    function playStep () {
+
+      kick.start(0);
+      // console.log('KICK');
+      // let thsstp = stp % 16;
+      // if (steps[thsstp]) kick.start(0);
+      // stp++;
+
+    }
+  }
+
 
   function renderSteps () {
     const arr = [];
@@ -28,6 +61,18 @@ export function Drums () {
       {/* <div className="drums-container" >
       </div> */}
       {renderSteps()}
+      <div onClick={() => {
+        console.log('CLICKED');
+        const sampler = new Tone.Sampler({
+          urls: {
+            A1: kick,
+          },
+          onload: () => {
+            sampler.triggerAttackRelease("A1", 0.5);
+          }
+        }).toDestination();
+      }}>START</div>
+
 
     </>
   )
