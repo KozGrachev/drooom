a65c982da4101e9e49660c820af33ca6910be1b0
Konstantin Grachev <kgrachev@gmail.com>
2020-12-08 20:28:16 +0000

    Moved the transport to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,48 @@
 import './style/App.scss';
+import { useContext } from 'react';
 import { Drums } from './drums'
+import * as Tone from 'tone';
+Tone.Transport.bpm.value = 120;
+Tone.Transport.swing = 0.08;
+// Tone.Transport.loop = true;
+let count = 0;
+let drumLoop;
 
-function App() {
+function App () {
+
+
+  function playPause () {
+    console.log('playing? ', Tone.Transport.state)
+    console.log('CLICKED');
+    if (Tone.Transport.state === 'stopped') {
+      console.log('STARTED');
+      Tone.Transport.toggle();
+      Tone.Transport.scheduleRepeat((time) => {
+        drumLoop(time, count);
+        count = (count + 1) % 16;
+        console.log('count', count);
+      }, '16n');
+      Tone.start();
+    } else {
+      // console.log('Stopped');
+      // setCount(0);
+      Tone.Transport.stop();
+      Tone.Transport.cancel();
+      count = 0;
+      console.log('STOPPED');
+    }
+    console.log('playing? ', Tone.Transport.state)
+    console.log('------------------ \n');
+  }
+
+  function setDrumLoop (cb) {
+    drumLoop = cb;
+  }
 
   return (
     <div>
-      <Drums />
+      <button id="playPause" onClick={() => playPause()}>PLAY PATTERN</button>
+      <Drums passUpLoop={setDrumLoop} />
     </div>
   );
 }
--- client/src/drums.js
+++ client/src/drums.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Step } from './Step';
 import './style/drums.scss';
 import * as Tone from 'tone';
@@ -12,11 +12,14 @@ const notes = { 'A1': kick, 'B1': snare, 'C1': perc, 'D1': chh, 'E1': ohh };
 const noteNames = { 'A1': 'kick', 'B1': 'snare', 'C1': 'perc', 'D1': 'chh', 'E1': 'ohh' };
 const notesEntries = Object.entries(noteNames);
 const sampler = new Tone.Sampler(notes).toDestination();
-Tone.Transport.bpm.value = 120;
-Tone.Transport.swing = 0.08;
-let count = 0;
 
-export function Drums () {
+// let count = 0;
+
+
+//* useEffect to rerender when Transport state changes (or on mount -->[])
+//* restart count on play
+
+export function Drums (props) {
 
   const [pattern, setPattern] = useState({
     kick: Array(16).fill(false),
@@ -26,6 +29,11 @@ export function Drums () {
     perc: Array(16).fill(false),
   });
 
+  useEffect(() => {
+    //* send the repeat function to the transport
+    props.passUpLoop(repeat);
+  }, []);
+
   function handleNoteClick (note) {
     setPattern((pat) => {
       pat[note.name][note.stepNum] = !pat[note.name][note.stepNum];
@@ -34,38 +42,16 @@ export function Drums () {
     console.log(pattern[note.name][note.stepNum]);
   }
 
-  function playPause () {
-    console.log('playing? ', Tone.Transport.state)
-    if (Tone.Transport.state === 'stopped') {
-      console.log('PLAYING?');
-      Tone.Transport.toggle();
-      Tone.Transport.scheduleRepeat(repeat, '16n');
-      Tone.start();
-    } else {
-      // console.log('Stopped');
-      // setCount(0);
-      Tone.Transport.stop();
-      Tone.Transport.cancel();
-      count = 0;
-      console.log('playing? ', Tone.Transport.state)
-    }
-  }
-
-  function repeat (time) {
-    console.log(count);
-    // count 0-15
-    // console.log('kick', pattern['kick'][step], 'snare', pattern['snare'][step], 'chh', pattern['chh'][step], 'ohh', pattern['ohh'][step], 'perc', pattern['perc'][step]);
+  function repeat (time, count) {
     for (const [note, drum] of notesEntries) {
-      // check each checkbox in each row by selecting the number corresponding to the step count
-      // const drumName = '.' + drum.match(/[a-z]+/);
       if (pattern[drum][count]) {
         //if checked, play
         sampler.triggerAttackRelease(note, '16n', time);
       }
     }
-    count = (count + 1) % 16;
-    // count++;
   }
+
+  console.log('RERENDERED');
 
   function checkRerender () {
     console.log('RERENDERED');
@@ -82,7 +68,7 @@ export function Drums () {
   return (
     <>
       {renderSteps()}
-      <button id="playPause" onClick={() => playPause()}>PLAY PATTERN</button>
+
       {checkRerender()}
     </>
   )
