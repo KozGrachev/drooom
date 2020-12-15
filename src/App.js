f46b3726866f8d9aa3bfde8c7eb3a1d8ac4f9bb0
Konstantin Grachev <kgrachev@gmail.com>
2020-12-15 08:13:55 +0000

    Purple color scheme

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import './style/App.scss';
 import { Drums } from './drums'
 import * as Tone from 'tone';
+import { useState } from 'react';
 Tone.Transport.bpm.value = 120;
 Tone.Transport.swing = 0.08;
 let count = 0;
 let drumLoop;
+let playing = false;
 
 function App () {
+  const [playing, setPlaying] = useState(false);
 
   function playPause () {
+    // setPlaying(p => !p);
+    // playing = playing ? false : true;
     if (Tone.Transport.state === 'stopped') {
       Tone.Transport.toggle();
-      Tone.Transport.scheduleRepeat( nextStep , '16n');
+      Tone.Transport.scheduleRepeat(nextStep, '16n');
       Tone.start();
     } else {
       Tone.Transport.stop();
