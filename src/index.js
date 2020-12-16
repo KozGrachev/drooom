431e65fedd2e58bc54d826002a8b5dcbc8893d24
Konstantin Grachev <kgrachev@gmail.com>
2020-12-16 23:09:32 +0000

    Initiated chakra

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import './style/index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ChakraProvider } from "@chakra-ui/react"
 
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App/>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
